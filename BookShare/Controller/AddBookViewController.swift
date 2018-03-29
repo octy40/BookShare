73ab2e5c407d6ca9c217d5126e35e1cc2ceda4ae
Octave Muhirwa <omuhirwa@lt-076.corp.localytics.com>
2018-03-29 09:06:11 -0400

    Add book functionality continued

--- BookShare/Controller/AddBookViewController.swift
+++ BookShare/Controller/AddBookViewController.swift
@@ -69,25 +69,76 @@ class AddBookViewController: UIViewController, UIImagePickerControllerDelegate, 
     */
     
     @IBAction func addBookBtnPressed(_ sender: Any) {
-        
         let bookUUID = NSUUID().uuidString
         let uid = Auth.auth().currentUser?.uid
-
-        let userData: [String: Any] = ["author":bookAuthor.text,
-                        "available":"yes",
-                        "numberOfPages":bookNumberOfPages.text,
-                        "owner":uid,
-                        "title":bookTitle.text]
         
+        // Save image to Storage
+        saveImageToStorage(bookUUID: bookUUID, image: imageView.image!)
+        // Get imageURL
+        let url = StorageService.ss.generateImageURL(bookUUID: bookUUID)
+        // Construct userData
+        let userData: [String: Any] = ["author":bookAuthor.text!,
+                        "available":"yes",
+                        "numberOfPages":bookNumberOfPages.text!,
+                        "owner":uid!,
+                        "title":bookTitle.text!,
+                        "imageURL": url]
+        // Create database entry
         DataService.ds.createFirebaseDBBook(bookUUID: bookUUID, userData: userData)
+        // Show book added success
+        let alertController = UIAlertController(title: "Success", message: "Book was added successfully", preferredStyle: UIAlertControllerStyle.alert)
+
+        let action = UIAlertAction.init(title: "Dismiss", style: UIAlertActionStyle.default) { (action) in
+            self.performSegue(withIdentifier: "goToBookShelf", sender: nil)
+        }
+        alertController.addAction(action)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
     
     // [START save image to storage and get URL]
-    func getImageURL(bookUUID: String){
-        
-    }
     
-    func saveImageToStorage(){
+    // Get StorageUploadTask object and control upload from this view controller
+    func saveImageToStorage(bookUUID: String, image: UIImage){
+        let uploadTask = StorageService.ss.uploadImageToStorage(imageUUID: bookUUID, image: image)
+        
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.observe(.resume) { snapshot in
+            // Upload resumed, also fires when the upload starts
+        }
+        
+        uploadTask.observe(.pause) { snapshot in
+            // Upload paused
+        }
+        
+        uploadTask.observe(.success) { snapshot in
+            // Upload completed successfully
+        }
+        
+        uploadTask.observe(.failure) { snapshot in
+            if let error = snapshot.error as NSError? {
+                switch (StorageErrorCode(rawValue: error.code)!) {
+                case .objectNotFound:
+                    // File doesn't exist
+                    break
+                case .unauthorized:
+                    // User doesn't have permission to access file
+                    break
+                case .cancelled:
+                    // User canceled the upload
+                    break
+                    
+                    /* ... */
+                    
+                case .unknown:
+                    // Unknown error occurred, inspect the server response
+                    break
+                default:
+                    // A separate error occurred. This is a good place to retry the upload.
+                    break
+                }
+            }
+        }
         
     }
     // [END save image to storage and get URL]
--- BookShare/Services/StorageService.swift
+++ BookShare/Services/StorageService.swift
@@ -22,7 +22,7 @@ class StorageService {
     }
     
     // [START upload image to storage]
-    func uploadImageToStorage(imageUUID: String, image: UIImage){
+    func uploadImageToStorage(imageUUID: String, image: UIImage) -> StorageUploadTask {
         // Convert image to jpg
         var data = Data()
         data = UIImageJPEGRepresentation(image, 0.8)!
@@ -37,45 +37,60 @@ class StorageService {
         // Upload file and metadata
         let uploadTask = imageRef.putData(data, metadata: metadata)
         
-        // TODO: Implement error handling if file fails to upload
-        // Listen for state changes, errors, and completion of the upload.
-        uploadTask.observe(.resume) { snapshot in
-            // Upload resumed, also fires when the upload starts
-        }
+        return uploadTask
         
-        uploadTask.observe(.pause) { snapshot in
-            // Upload paused
-        }
+//        // TODO: Implement error handling if file fails to upload
+//        // Listen for state changes, errors, and completion of the upload.
+//        uploadTask.observe(.resume) { snapshot in
+//            // Upload resumed, also fires when the upload starts
+//        }
+//
+//        uploadTask.observe(.pause) { snapshot in
+//            // Upload paused
+//        }
+//
+//        uploadTask.observe(.success) { snapshot in
+//            // Upload completed successfully
+//        }
+//
+//        uploadTask.observe(.failure) { snapshot in
+//            if let error = snapshot.error as NSError? {
+//                switch (StorageErrorCode(rawValue: error.code)!) {
+//                case .objectNotFound:
+//                    // File doesn't exist
+//                    break
+//                case .unauthorized:
+//                    // User doesn't have permission to access file
+//                    break
+//                case .cancelled:
+//                    // User canceled the upload
+//                    break
+//
+//                    /* ... */
+//
+//                case .unknown:
+//                    // Unknown error occurred, inspect the server response
+//                    break
+//                default:
+//                    // A separate error occurred. This is a good place to retry the upload.
+//                    break
+//                }
+//            }
+//        }
         
-        uploadTask.observe(.success) { snapshot in
-            // Upload completed successfully
-        }
-        
-        uploadTask.observe(.failure) { snapshot in
-            if let error = snapshot.error as NSError? {
-                switch (StorageErrorCode(rawValue: error.code)!) {
-                case .objectNotFound:
-                    // File doesn't exist
-                    break
-                case .unauthorized:
-                    // User doesn't have permission to access file
-                    break
-                case .cancelled:
-                    // User canceled the upload
-                    break
-                    
-                    /* ... */
-                    
-                case .unknown:
-                    // Unknown error occurred, inspect the server response
-                    break
-                default:
-                    // A separate error occurred. This is a good place to retry the upload.
-                    break
-                }
+    }
+    
+    func generateImageURL(bookUUID: String) -> String {
+        let imageRef = REF_IMAGES.child(bookUUID + ".jpg")
+        var returnVal = ""
+        imageRef.downloadURL { (url, error) in
+            if let error = error {
+                print("BookShare: Download error snag \(error)")
+            } else {
+                returnVal = (url?.absoluteString)!
             }
         }
-        
+        return returnVal
     }
     
 }
